1a884006062f2d65cb70bfb63864de9e9a27129f
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2025-01-08 18:50:49 -0300

    Usar o primitivo int restringe a quantidade de digitos, se fez necessário o uso do BigDecimal

--- src/main/java/com/nascimento/dio/iniciante/challenge2867/Digitos.java
+++ src/main/java/com/nascimento/dio/iniciante/challenge2867/Digitos.java
@@ -1,13 +1,23 @@
 package com.nascimento.dio.iniciante.challenge2867;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Digitos {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int testes = entrada.nextInt();
+        for (int i = 0; i < testes; i++){
+            BigInteger valor1 = BigInteger.valueOf(entrada.nextInt());
+            int valor2 = entrada.nextInt();
+            BigInteger total = valor1.pow(valor2);
+            System.out.println(quantidadeDigitos(total));
 
-        System.out.println(quantidadeDigitos(123456));
+        }
     }
 
-    public static long quantidadeDigitos(long valor){
-        valor /= 10;
-        return valor == 0 ? 1 : 1 + quantidadeDigitos(valor);
+    public static BigInteger quantidadeDigitos(BigInteger valor){
+        valor = valor.divide(BigInteger.TEN);
+        return valor.equals(BigInteger.ZERO) ? BigInteger.ONE : BigInteger.ONE.add(quantidadeDigitos(valor));
     }
 }
